460c299b1d92d0476cba6d7a93b54461762d0bc1
Ephrem Demelash <demelashephrem@gmail.com>
2022-12-26 00:08:56 +0300

    feat: add darkmode icon to the nav bar component

--- components/NavBar.js
+++ components/NavBar.js
@@ -22,6 +22,26 @@ export function NavBar() {
             <NavLink key={title} title={title} link={link} />
           ))}
         </div>
+        <button
+          aria-label="Toggle Dark Mode"
+          type="button"
+          class=" flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-900/90 dark:ring-teal-500/50 dark:hover:ring-white/20"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            stroke="currentColor"
+            class="h-4 w-4 text-gray-800 dark:text-teal-500"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+            ></path>
+          </svg>
+        </button>
       </nav>
     </header>
   );
